eab5215a670799cce406feb57003a6b55f8fcf4c
Paulo José Maciel de Brito <paulojmbrito27@gmail.com>
2020-09-21 12:12:50 -0300

    Implementado tela cadastro aluno

--- HBSIS.BOLETIM.Front/Adiministrador/CadastraAlunoView.Designer.cs
+++ HBSIS.BOLETIM.Front/Adiministrador/CadastraAlunoView.Designer.cs
@@ -28,12 +28,176 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lbl_ok = new System.Windows.Forms.Label();
+            this.lbl_erro1 = new System.Windows.Forms.Label();
+            this.lbl_erro2 = new System.Windows.Forms.Label();
+            this.lbl_erro3 = new System.Windows.Forms.Label();
+            this.txt_nome = new System.Windows.Forms.TextBox();
+            this.txt_sobrenome = new System.Windows.Forms.TextBox();
+            this.txt_cpf = new System.Windows.Forms.TextBox();
+            this.calendar = new System.Windows.Forms.MonthCalendar();
+            this.btn_Cadastrar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(92, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nome";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(42, 78);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(114, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Sobrenome";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(113, 126);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(43, 25);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Cpf";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(103, 168);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 25);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Data";
+            // 
+            // lbl_ok
+            // 
+            this.lbl_ok.AutoSize = true;
+            this.lbl_ok.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_ok.Location = new System.Drawing.Point(79, 484);
+            this.lbl_ok.Name = "lbl_ok";
+            this.lbl_ok.Size = new System.Drawing.Size(0, 31);
+            this.lbl_ok.TabIndex = 4;
+            // 
+            // lbl_erro1
+            // 
+            this.lbl_erro1.AutoSize = true;
+            this.lbl_erro1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_erro1.Location = new System.Drawing.Point(79, 524);
+            this.lbl_erro1.Name = "lbl_erro1";
+            this.lbl_erro1.Size = new System.Drawing.Size(0, 31);
+            this.lbl_erro1.TabIndex = 5;
+            // 
+            // lbl_erro2
+            // 
+            this.lbl_erro2.AutoSize = true;
+            this.lbl_erro2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_erro2.Location = new System.Drawing.Point(79, 563);
+            this.lbl_erro2.Name = "lbl_erro2";
+            this.lbl_erro2.Size = new System.Drawing.Size(0, 31);
+            this.lbl_erro2.TabIndex = 6;
+            // 
+            // lbl_erro3
+            // 
+            this.lbl_erro3.AutoSize = true;
+            this.lbl_erro3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_erro3.Location = new System.Drawing.Point(79, 602);
+            this.lbl_erro3.Name = "lbl_erro3";
+            this.lbl_erro3.Size = new System.Drawing.Size(0, 31);
+            this.lbl_erro3.TabIndex = 7;
+            // 
+            // txt_nome
+            // 
+            this.txt_nome.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_nome.Location = new System.Drawing.Point(179, 24);
+            this.txt_nome.Name = "txt_nome";
+            this.txt_nome.Size = new System.Drawing.Size(275, 30);
+            this.txt_nome.TabIndex = 8;
+            // 
+            // txt_sobrenome
+            // 
+            this.txt_sobrenome.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_sobrenome.Location = new System.Drawing.Point(180, 73);
+            this.txt_sobrenome.Name = "txt_sobrenome";
+            this.txt_sobrenome.Size = new System.Drawing.Size(275, 30);
+            this.txt_sobrenome.TabIndex = 9;
+            // 
+            // txt_cpf
+            // 
+            this.txt_cpf.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_cpf.Location = new System.Drawing.Point(180, 121);
+            this.txt_cpf.Name = "txt_cpf";
+            this.txt_cpf.Size = new System.Drawing.Size(275, 30);
+            this.txt_cpf.TabIndex = 10;
+            // 
+            // calendar
+            // 
+            this.calendar.Location = new System.Drawing.Point(180, 168);
+            this.calendar.Name = "calendar";
+            this.calendar.TabIndex = 11;
+            // 
+            // btn_Cadastrar
+            // 
+            this.btn_Cadastrar.Location = new System.Drawing.Point(179, 387);
+            this.btn_Cadastrar.Name = "btn_Cadastrar";
+            this.btn_Cadastrar.Size = new System.Drawing.Size(265, 35);
+            this.btn_Cadastrar.TabIndex = 12;
+            this.btn_Cadastrar.Text = "CADASTRAR";
+            this.btn_Cadastrar.UseVisualStyleBackColor = true;
+            this.btn_Cadastrar.Click += new System.EventHandler(this.btn_Cadastrar_Click);
+            // 
+            // CadastraAlunoView
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "CadastraAlunoView";
+            this.ClientSize = new System.Drawing.Size(845, 645);
+            this.Controls.Add(this.btn_Cadastrar);
+            this.Controls.Add(this.calendar);
+            this.Controls.Add(this.txt_cpf);
+            this.Controls.Add(this.txt_sobrenome);
+            this.Controls.Add(this.txt_nome);
+            this.Controls.Add(this.lbl_erro3);
+            this.Controls.Add(this.lbl_erro2);
+            this.Controls.Add(this.lbl_erro1);
+            this.Controls.Add(this.lbl_ok);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "CadastraAlunoView";
+            this.Text = "Cadastro de aluno";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lbl_ok;
+        private System.Windows.Forms.Label lbl_erro1;
+        private System.Windows.Forms.Label lbl_erro2;
+        private System.Windows.Forms.Label lbl_erro3;
+        private System.Windows.Forms.TextBox txt_nome;
+        private System.Windows.Forms.TextBox txt_sobrenome;
+        private System.Windows.Forms.TextBox txt_cpf;
+        private System.Windows.Forms.MonthCalendar calendar;
+        private System.Windows.Forms.Button btn_Cadastrar;
     }
 }
--- HBSIS.BOLETIM.Front/Adiministrador/CadastraAlunoView.cs
+++ HBSIS.BOLETIM.Front/Adiministrador/CadastraAlunoView.cs
@@ -1,9 +1,13 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using PROJETO_HBSIS.BOLETIM.MODELS;
+using PROJETO_HBSIS.BOLETIM.MODELS.RESULTS;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -15,6 +19,43 @@ namespace HBSIS.BOLETIM.Front.Adiministrador
         public CadastraAlunoView()
         {
             InitializeComponent();
+        }
+
+        private void btn_Cadastrar_Click(object sender, EventArgs e)
+        {
+            lbl_ok.Text = "";
+            lbl_erro1.Text = "";
+            lbl_erro2.Text = "";
+            lbl_erro3.Text = "";
+
+            Aluno aluno = new Aluno() {
+                Nome = txt_nome.Text,
+                Sobrenome = txt_sobrenome.Text,
+                Cpf = txt_cpf.Text,
+                DataNascimento = calendar.SelectionStart.Date
+            };
+            
+
+            var client = new HttpClient();
+            client.BaseAddress = new Uri("https://localhost:44303/");
+            var request = client.PostAsync("Alunos/Cadastrar", new StringContent(JsonConvert.SerializeObject(aluno), System.Text.Encoding.UTF8, "application/json"));
+            request.Wait();
+            var result = request.Result.Content.ReadAsStringAsync();
+            var resultado = JsonConvert.DeserializeObject<PadraoResult<Aluno>>(result.Result);
+
+            if (resultado.Error)
+            {
+                lbl_erro1.Text = resultado.Message[0];
+                lbl_erro2.Text = resultado.Message.Count > 1 ? resultado.Message[1] : "";
+                lbl_erro3.Text = resultado.Message.Count > 2 ? resultado.Message[2] : "";
+            }
+            else
+            {
+                lbl_ok.Text = $"Curso {aluno.Nome} cadastrado!";
+                lbl_erro1.Text = $"Login e senha padrão gerados automaticamente:";
+                lbl_erro2.Text = $"Login: {resultado.Data[0].Login}";
+                lbl_erro3.Text = $"Senha: {resultado.Data[0].Password}";
+            }
         }
     }
 }
