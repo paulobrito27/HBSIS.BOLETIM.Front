21e4d2b6d220be902cf2d90692f506bf51cda334
Paulo José Maciel de Brito <paulojmbrito27@gmail.com>
2020-09-21 15:19:30 -0300

    quase la

--- HBSIS.BOLETIM.Front/Adiministrador/MatAlunoMateriaView.Designer.cs
+++ HBSIS.BOLETIM.Front/Adiministrador/MatAlunoMateriaView.Designer.cs
@@ -28,12 +28,168 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lbl_curso = new System.Windows.Forms.Label();
+            this.lbl_erro2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lbl_erro1 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.lbl_erro3 = new System.Windows.Forms.Label();
+            this.box_Aluno = new System.Windows.Forms.ComboBox();
+            this.box_Materia = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.lbl_ok = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(59, 66);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Aluno";
+            // 
+            // lbl_curso
+            // 
+            this.lbl_curso.AutoSize = true;
+            this.lbl_curso.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_curso.Location = new System.Drawing.Point(59, 128);
+            this.lbl_curso.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbl_curso.Name = "lbl_curso";
+            this.lbl_curso.Size = new System.Drawing.Size(87, 25);
+            this.lbl_curso.TabIndex = 1;
+            this.lbl_curso.Text = "Materias";
+            // 
+            // lbl_erro2
+            // 
+            this.lbl_erro2.AutoSize = true;
+            this.lbl_erro2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_erro2.Location = new System.Drawing.Point(58, 479);
+            this.lbl_erro2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbl_erro2.Name = "lbl_erro2";
+            this.lbl_erro2.Size = new System.Drawing.Size(0, 31);
+            this.lbl_erro2.TabIndex = 2;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(1375, 930);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(64, 25);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "label4";
+            // 
+            // lbl_erro1
+            // 
+            this.lbl_erro1.AutoSize = true;
+            this.lbl_erro1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_erro1.Location = new System.Drawing.Point(58, 416);
+            this.lbl_erro1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbl_erro1.Name = "lbl_erro1";
+            this.lbl_erro1.Size = new System.Drawing.Size(0, 31);
+            this.lbl_erro1.TabIndex = 4;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(1440, 1160);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(64, 25);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "label6";
+            // 
+            // lbl_erro3
+            // 
+            this.lbl_erro3.AutoSize = true;
+            this.lbl_erro3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_erro3.Location = new System.Drawing.Point(58, 533);
+            this.lbl_erro3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbl_erro3.Name = "lbl_erro3";
+            this.lbl_erro3.Size = new System.Drawing.Size(0, 31);
+            this.lbl_erro3.TabIndex = 6;
+            // 
+            // box_Aluno
+            // 
+            this.box_Aluno.FormattingEnabled = true;
+            this.box_Aluno.Location = new System.Drawing.Point(152, 66);
+            this.box_Aluno.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.box_Aluno.Name = "box_Aluno";
+            this.box_Aluno.Size = new System.Drawing.Size(478, 33);
+            this.box_Aluno.TabIndex = 7;
+            // 
+            // box_Materia
+            // 
+            this.box_Materia.FormattingEnabled = true;
+            this.box_Materia.Location = new System.Drawing.Point(152, 125);
+            this.box_Materia.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.box_Materia.Name = "box_Materia";
+            this.box_Materia.Size = new System.Drawing.Size(478, 33);
+            this.box_Materia.TabIndex = 8;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(461, 187);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(169, 48);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Matricular";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lbl_ok
+            // 
+            this.lbl_ok.AutoSize = true;
+            this.lbl_ok.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_ok.Location = new System.Drawing.Point(58, 358);
+            this.lbl_ok.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbl_ok.Name = "lbl_ok";
+            this.lbl_ok.Size = new System.Drawing.Size(0, 31);
+            this.lbl_ok.TabIndex = 10;
+            // 
+            // MatAlunoMateriaView
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "MatAlunoMateriaView";
+            this.ClientSize = new System.Drawing.Size(814, 637);
+            this.Controls.Add(this.lbl_ok);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.box_Materia);
+            this.Controls.Add(this.box_Aluno);
+            this.Controls.Add(this.lbl_erro3);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.lbl_erro1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lbl_erro2);
+            this.Controls.Add(this.lbl_curso);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "MatAlunoMateriaView";
+            this.Text = "Habilita Materia para Aluno";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lbl_curso;
+        private System.Windows.Forms.Label lbl_erro2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lbl_erro1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label lbl_erro3;
+        private System.Windows.Forms.ComboBox box_Aluno;
+        private System.Windows.Forms.ComboBox box_Materia;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lbl_ok;
     }
 }
--- HBSIS.BOLETIM.Front/Adiministrador/MatAlunoMateriaView.cs
+++ HBSIS.BOLETIM.Front/Adiministrador/MatAlunoMateriaView.cs
@@ -1,9 +1,15 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using PROJETO_HBSIS.BOLETIM.MODELS;
+using PROJETO_HBSIS.BOLETIM.MODELS.ClassesAssociativas;
+using PROJETO_HBSIS.BOLETIM.MODELS.RESULTS;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net.Http;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -15,6 +21,75 @@ namespace HBSIS.BOLETIM.Front.Adiministrador
         public MatAlunoMateriaView()
         {
             InitializeComponent();
+            Popula();
+        }
+
+        private void Popula()
+        {
+
+            var client = new HttpClient();
+            client.BaseAddress = new Uri("https://localhost:44303/");
+            var request = client.GetAsync("Alunos");
+            request.Wait();
+            var result = request.Result.Content.ReadAsStringAsync();
+            var resultado = JsonConvert.DeserializeObject<PadraoResult<Aluno>>(result.Result);
+
+            box_Aluno.Items.Clear();
+            foreach (var item in resultado.Data)
+            {
+                if (item.IdCurso != 0)
+                {
+                    box_Aluno.Items.Add($"{item.Id}: {item.Nome} {item.Sobrenome}");
+                }
+
+            }
+
+            request = client.GetAsync("Materias");
+            request.Wait();
+            result = request.Result.Content.ReadAsStringAsync();
+            var resultado2 = JsonConvert.DeserializeObject<PadraoResult<Materia>>(result.Result);
+
+            box_Materia.Items.Clear();
+            foreach (var item in resultado2.Data)
+            {
+
+                box_Materia.Items.Add($"{item.Id}: {item.Nome} ");
+            }
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            lbl_ok.Text = "";
+            lbl_erro1.Text = "";
+            lbl_erro2.Text  ="";
+            lbl_erro3.Text  ="";
+
+            var id_aluno = box_Aluno.SelectedItem.ToString().Split(':');
+            var id_materia = box_Materia.SelectedItem.ToString().Split(':');
+
+            (int, int) itens = (Convert.ToInt32(id_aluno[0]), Convert.ToInt32(id_materia[0]));
+
+
+            var client = new HttpClient();
+            client.BaseAddress = new Uri("https://localhost:44303/");
+            var request = client.PostAsync("Alunos/MatricularAlunoEmMateria", new StringContent(JsonConvert.SerializeObject(itens), System.Text.Encoding.UTF8, "application/json"));
+            request.Wait();
+            var result = request.Result.Content.ReadAsStringAsync();
+            var resultado = JsonConvert.DeserializeObject<PadraoResult<Aluno>>(result.Result);
+
+            if (resultado.Error)
+            {
+                lbl_erro1.Text = resultado.Message[0];
+                lbl_erro2.Text = resultado.Message.Count > 1 ? resultado.Message[1] : "";
+                lbl_erro3.Text = resultado.Message.Count > 2 ? resultado.Message[2] : "";
+            }
+            else
+            {
+                lbl_ok.Text = $"Aluno {resultado.Data[0].Nome} matriculado!";
+                
+            }
         }
     }
 }
