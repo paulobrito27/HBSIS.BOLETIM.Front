edf5ee952b32a7f4574243ccbae790cdade17c96
Paulo José Maciel de Brito <paulojmbrito27@gmail.com>
2020-09-22 07:01:59 -0300

    correção

--- HBSIS.BOLETIM.Front/ProfessorView.cs
+++ HBSIS.BOLETIM.Front/ProfessorView.cs
@@ -28,48 +28,75 @@ namespace HBSIS.BOLETIM.Front
             lbl_erro3.Text = "";
             lbl_ok.Text = "";
 
-            (int, int, double) dados;
-            
-            var id_aluno = boxAluno.SelectedItem.ToString().Split(':');
-            var id_materia = boxMateria.SelectedItem.ToString().Split(':');
-            var nota = txtNota.Text;
-            dados.Item1 = Convert.ToInt32(id_aluno[0]);
-            dados.Item2 = Convert.ToInt32(id_materia[0]);
-            dados.Item3 = Convert.ToDouble(nota);
-
-            var client = new HttpClient();
-            client.BaseAddress = new Uri("https://localhost:44303/");
-            var request = client.PostAsync("Alunos/AtribuirNotaAluno", new StringContent(JsonConvert.SerializeObject(dados), System.Text.Encoding.UTF8, "application/json"));
-            request.Wait();
-            var result = request.Result.Content.ReadAsStringAsync();
-            var resultado = JsonConvert.DeserializeObject<PadraoResult<Curso>>(result.Result);
-
-            if (resultado.Error)
+            try
             {
-                lbl_erro1.Text = resultado.Message[0];
-                lbl_erro2.Text = resultado.Message.Count > 1 ? resultado.Message[1] : "";
-                lbl_erro3.Text = resultado.Message.Count > 2 ? resultado.Message[2] : "";
+
+                if (boxAluno.SelectedItem != null && boxMateria.SelectedItem != null)
+                {
+                    (int, int, double) dados;
+
+                    var id_aluno = boxAluno.SelectedItem.ToString().Split(':');
+                    var id_materia = boxMateria.SelectedItem.ToString().Split(':');
+                    var nota = txtNota.Text;
+
+
+                    dados.Item1 = Convert.ToInt32(id_aluno[0]);
+                    dados.Item2 = Convert.ToInt32(id_materia[0]);
+                    dados.Item3 = Convert.ToDouble(nota);
+
+                    var client = new HttpClient();
+                    client.BaseAddress = new Uri("https://localhost:44303/");
+                    var request = client.PostAsync("Alunos/AtribuirNotaAluno", new StringContent(JsonConvert.SerializeObject(dados), System.Text.Encoding.UTF8, "application/json"));
+                    request.Wait();
+                    var result = request.Result.Content.ReadAsStringAsync();
+                    var resultado = JsonConvert.DeserializeObject<PadraoResult<Curso>>(result.Result);
+
+                    if (resultado.Error)
+                    {
+                        lbl_erro1.Text = resultado.Message[0];
+                        lbl_erro2.Text = resultado.Message.Count > 1 ? resultado.Message[1] : "";
+                        lbl_erro3.Text = resultado.Message.Count > 2 ? resultado.Message[2] : "";
+                    }
+                    else
+                    {
+                        lbl_ok.Text = $"Nota Atribuida !";
+                    }
+
+                }
+                else
+                {
+                    lbl_erro1.Text = "Selecione  um aluno e uma materia";
+                }
             }
-            else
+            catch (Exception msg)
             {
-                lbl_ok.Text = $"Nota Atribuida !";
+                lbl_erro1.Text = msg.Message;
             }
-          
+
         }
 
 
         private void boxAluno_SelectedIndexChanged(object sender, EventArgs e)
         {
             boxMateria.Items.Clear();
-            var id_aluno = boxAluno.SelectedItem.ToString().Split(':');
-            int id = Convert.ToInt32(id_aluno[0]);
 
-            Aluno aluno = listaAlunos.Where(q => q.Id == id).FirstOrDefault();
-
-            foreach(var materia in aluno.Curso.Materias)
+            if (boxAluno.SelectedItem != null)
             {
-                boxMateria.Items.Add($"{materia.Materia.Id}: {materia.Materia.Nome}");
+                var id_aluno = boxAluno.SelectedItem.ToString().Split(':');
+                int id = Convert.ToInt32(id_aluno[0]);
+
+                Aluno aluno = listaAlunos.Where(q => q.Id == id).FirstOrDefault();
+
+                foreach (var materia in aluno.Curso.Materias)
+                {
+                    boxMateria.Items.Add($"{materia.Materia.Id}: {materia.Materia.Nome}");
+                }
             }
+            else
+            {
+                lbl_erro1.Text = "Selecione um aluno";
+            }
+
         }
 
 
