94b084fcedf91fda121df5f65d6271d7b8ff14e1
Paulo José Maciel de Brito <paulojmbrito27@gmail.com>
2020-09-22 07:11:26 -0300

    correção2

--- HBSIS.BOLETIM.Front/Adiministrador/CadastraMateriaView.cs
+++ HBSIS.BOLETIM.Front/Adiministrador/CadastraMateriaView.cs
@@ -2,14 +2,7 @@
 using PROJETO_HBSIS.BOLETIM.MODELS;
 using PROJETO_HBSIS.BOLETIM.MODELS.RESULTS;
 using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
 using System.Net.Http;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace HBSIS.BOLETIM.Front.Adiministrador
@@ -24,43 +17,52 @@ namespace HBSIS.BOLETIM.Front.Adiministrador
 
         private void btn_cadastrar_Click(object sender, EventArgs e)
         {
-            lbl_ok.Text = "";
-            lbl_erro1.Text = "";
-            lbl_erro2.Text = "";
-            lbl_erro3.Text = "";
 
-            Materia materia = new Materia() { Nome = txt_nome.Text };
-            materia.Descricao = txt_descricao.Text;
-            materia.DataCadastro = calendar.SelectionStart.Date;
-
-            if (comboBox.SelectedItem == "Inativo")
+            try
             {
-                materia.Situacao = PROJETO_HBSIS.BOLETIM.MODELS.Enum.StatusMateriaEnum.INATIVO;
-            }
-            if (comboBox.SelectedItem == "Ativo")
-            {
-                materia.Situacao = PROJETO_HBSIS.BOLETIM.MODELS.Enum.StatusMateriaEnum.ATIVO;
-            }
+                lbl_ok.Text = "";
+                lbl_erro1.Text = "";
+                lbl_erro2.Text = "";
+                lbl_erro3.Text = "";
+
+                Materia materia = new Materia() { Nome = txt_nome.Text };
+                materia.Descricao = txt_descricao.Text;
+                materia.DataCadastro = calendar.SelectionStart.Date;
+
+                if (comboBox.SelectedItem == "Inativo")
+                {
+                    materia.Situacao = PROJETO_HBSIS.BOLETIM.MODELS.Enum.StatusMateriaEnum.INATIVO;
+                }
+                if (comboBox.SelectedItem == "Ativo")
+                {
+                    materia.Situacao = PROJETO_HBSIS.BOLETIM.MODELS.Enum.StatusMateriaEnum.ATIVO;
+                }
 
 
-            var client = new HttpClient();
-            client.BaseAddress = new Uri("https://localhost:44303/");
-            var request = client.PostAsync("Materias/Cadastrar", new StringContent(JsonConvert.SerializeObject(materia), System.Text.Encoding.UTF8, "application/json"));
-            request.Wait();
-            var result = request.Result.Content.ReadAsStringAsync();
-            var resultado = JsonConvert.DeserializeObject<PadraoResult<Materia>>(result.Result);
+                var client = new HttpClient();
+                client.BaseAddress = new Uri("https://localhost:44303/");
+                var request = client.PostAsync("Materias/Cadastrar", new StringContent(JsonConvert.SerializeObject(materia), System.Text.Encoding.UTF8, "application/json"));
+                request.Wait();
+                var result = request.Result.Content.ReadAsStringAsync();
+                var resultado = JsonConvert.DeserializeObject<PadraoResult<Materia>>(result.Result);
 
-            if (resultado.Error)
-            {
-                lbl_erro1.Text = resultado.Message[0];
-                lbl_erro2.Text = resultado.Message.Count > 1 ? resultado.Message[1] : "";
-                lbl_erro3.Text = resultado.Message.Count > 2 ? resultado.Message[2] : "";
+                if (resultado.Error)
+                {
+                    lbl_erro1.Text = resultado.Message[0];
+                    lbl_erro2.Text = resultado.Message.Count > 1 ? resultado.Message[1] : "";
+                    lbl_erro3.Text = resultado.Message.Count > 2 ? resultado.Message[2] : "";
+                }
+                else
+                {
+                    lbl_ok.Text = $"Materia {materia.Nome} cadastrada!";
+                    PopularDados();
+                }
             }
-            else
+            catch (Exception msg)
             {
-                lbl_ok.Text = $"Materia {materia.Nome} cadastrada!";
-                PopularDados();
+                lbl_erro1.Text = msg.Message;
             }
+
         }
 
         private void btn_deletar_Click(object sender, EventArgs e)
@@ -103,52 +105,60 @@ namespace HBSIS.BOLETIM.Front.Adiministrador
 
         private void btn_alterar_Click(object sender, EventArgs e)
         {
-            lbl_ok.Text = "";
-            lbl_erro1.Text = "";
-            lbl_erro2.Text = "";
-            lbl_erro3.Text = "";
-
-            Materia materia = new Materia() { Nome = txt_nome.Text };
-            materia.Descricao = txt_descricao.Text;
-            materia.DataCadastro = calendar.SelectionStart.Date;
-
-            if (txt_id.Text != "")
+            try
             {
-                materia.Id = Convert.ToInt32(txt_id.Text);
+                lbl_ok.Text = "";
+                lbl_erro1.Text = "";
+                lbl_erro2.Text = "";
+                lbl_erro3.Text = "";
 
-                if (comboBox.SelectedItem == "Inativo")
+                Materia materia = new Materia() { Nome = txt_nome.Text };
+                materia.Descricao = txt_descricao.Text;
+                materia.DataCadastro = calendar.SelectionStart.Date;
+
+                if (txt_id.Text != "")
                 {
-                    materia.Situacao = PROJETO_HBSIS.BOLETIM.MODELS.Enum.StatusMateriaEnum.INATIVO;
-                }
-                if (comboBox.SelectedItem == "Ativo")
-                {
-                    materia.Situacao = PROJETO_HBSIS.BOLETIM.MODELS.Enum.StatusMateriaEnum.ATIVO;
-                }
+                    materia.Id = Convert.ToInt32(txt_id.Text);
+
+                    if (comboBox.SelectedItem == "Inativo")
+                    {
+                        materia.Situacao = PROJETO_HBSIS.BOLETIM.MODELS.Enum.StatusMateriaEnum.INATIVO;
+                    }
+                    if (comboBox.SelectedItem == "Ativo")
+                    {
+                        materia.Situacao = PROJETO_HBSIS.BOLETIM.MODELS.Enum.StatusMateriaEnum.ATIVO;
+                    }
 
 
-                var client = new HttpClient();
-                client.BaseAddress = new Uri("https://localhost:44303/");
-                var request = client.PostAsync("Materias/Alterar", new StringContent(JsonConvert.SerializeObject(materia), System.Text.Encoding.UTF8, "application/json"));
-                request.Wait();
-                var result = request.Result.Content.ReadAsStringAsync();
-                var resultado = JsonConvert.DeserializeObject<PadraoResult<Materia>>(result.Result);
+                    var client = new HttpClient();
+                    client.BaseAddress = new Uri("https://localhost:44303/");
+                    var request = client.PostAsync("Materias/Alterar", new StringContent(JsonConvert.SerializeObject(materia), System.Text.Encoding.UTF8, "application/json"));
+                    request.Wait();
+                    var result = request.Result.Content.ReadAsStringAsync();
+                    var resultado = JsonConvert.DeserializeObject<PadraoResult<Materia>>(result.Result);
 
-                if (resultado.Error)
-                {
-                    lbl_erro1.Text = resultado.Message[0];
-                    lbl_erro2.Text = resultado.Message.Count > 1 ? resultado.Message[1] : "";
-                    lbl_erro3.Text = resultado.Message.Count > 2 ? resultado.Message[2] : "";
+                    if (resultado.Error)
+                    {
+                        lbl_erro1.Text = resultado.Message[0];
+                        lbl_erro2.Text = resultado.Message.Count > 1 ? resultado.Message[1] : "";
+                        lbl_erro3.Text = resultado.Message.Count > 2 ? resultado.Message[2] : "";
+                    }
+                    else
+                    {
+                        lbl_ok.Text = $"Materia {materia.Nome} atualizada!";
+                        PopularDados();
+                    }
                 }
                 else
                 {
-                    lbl_ok.Text = $"Materia {materia.Nome} atualizada!";
-                    PopularDados();
+                    lbl_erro1.Text = "Informe o id";
                 }
             }
-            else
+            catch (Exception msg)
             {
-                lbl_erro1.Text = "Informe o id";
+                lbl_erro1.Text = msg.Message;
             }
+
         }
 
 
@@ -211,7 +221,7 @@ namespace HBSIS.BOLETIM.Front.Adiministrador
                     }
                 }
 
-            }  
+            }
 
         }
 
@@ -219,27 +229,36 @@ namespace HBSIS.BOLETIM.Front.Adiministrador
         {
             (string, string) dados;
 
-            dados.Item1 = comboCurso.SelectedItem.ToString();
-            dados.Item2 = comboMateria.SelectedItem.ToString();
 
-            var client = new HttpClient();
-            client.BaseAddress = new Uri("https://localhost:44303/");
-            var request = client.PostAsync("Cursos/AddMateria", new StringContent(JsonConvert.SerializeObject(dados), System.Text.Encoding.UTF8, "application/json"));
-            request.Wait();
-            var result = request.Result.Content.ReadAsStringAsync();
-            var resultado = JsonConvert.DeserializeObject<PadraoResult<Curso>>(result.Result);
-
-            if (resultado.Error)
+            if(comboCurso.SelectedItem != null && comboMateria.SelectedItem != null)
             {
-                lbl_erro1.Text = resultado.Message[0];
-                lbl_erro2.Text = resultado.Message.Count > 1 ? resultado.Message[1] : "";
-                lbl_erro3.Text = resultado.Message.Count > 2 ? resultado.Message[2] : "";
+                dados.Item1 = comboCurso.SelectedItem.ToString();
+                dados.Item2 = comboMateria.SelectedItem.ToString();
+
+                var client = new HttpClient();
+                client.BaseAddress = new Uri("https://localhost:44303/");
+                var request = client.PostAsync("Cursos/AddMateria", new StringContent(JsonConvert.SerializeObject(dados), System.Text.Encoding.UTF8, "application/json"));
+                request.Wait();
+                var result = request.Result.Content.ReadAsStringAsync();
+                var resultado = JsonConvert.DeserializeObject<PadraoResult<Curso>>(result.Result);
+
+                if (resultado.Error)
+                {
+                    lbl_erro1.Text = resultado.Message[0];
+                    lbl_erro2.Text = resultado.Message.Count > 1 ? resultado.Message[1] : "";
+                    lbl_erro3.Text = resultado.Message.Count > 2 ? resultado.Message[2] : "";
+                }
+                else
+                {
+                    lbl_ok.Text = $"Materia  vinculada!";
+                    PopularDados();
+                }
             }
             else
             {
-                lbl_ok.Text = $"Materia  vinculada!";
-                PopularDados();
+                lbl_erro1.Text = "Selecione materia e curso!";
             }
+          
         }
     }
 }
