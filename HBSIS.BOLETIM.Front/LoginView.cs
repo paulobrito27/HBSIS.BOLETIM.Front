2172333071a5fe22928272df85f3ee6a71f41b91
Paulo José Maciel de Brito <paulojmbrito27@gmail.com>
2020-09-22 07:22:31 -0300

    jj

--- HBSIS.BOLETIM.Front/LoginView.cs
+++ HBSIS.BOLETIM.Front/LoginView.cs
@@ -20,52 +20,61 @@ namespace HBSIS.BOLETIM.Front
 
         private void btn_logar_Click(object sender, EventArgs e)
         {
-            var login = txt_login.Text;
-            var password = txt_Senha.Text;
-            user = new Usuario
+
+            try
             {
-                Login = login,
-                Password = password
-            };
-
-            var client = new HttpClient();
-            client.BaseAddress = new Uri("https://localhost:44303/");
-            var request = client.PostAsync("Login/", new StringContent(JsonConvert.SerializeObject(user), System.Text.Encoding.UTF8, "application/json"));
-            request.Wait();
-            var result = request.Result.Content.ReadAsStringAsync();
-            result.Wait();
-            var resultado = JsonConvert.DeserializeObject<PadraoResult<Usuario>>(result.Result);
-
-            if (!resultado.Error)
-            {
-                user = resultado.Data[0];
-                this.Close();
-                if (user.TipoUsuario == TipoUsuarioEnum.ALUNO)
+                var login = txt_login.Text;
+                var password = txt_Senha.Text;
+                user = new Usuario
                 {
-                    t1 = new Thread(JanelaAluno);
-                }
-                if (user.TipoUsuario == TipoUsuarioEnum.ADMINISTRADOR)
-                {
-                    t1 = new Thread(JanelaAdministrador);
-                }
-                if (user.TipoUsuario == TipoUsuarioEnum.PROFESSOR)
-                {
-                    t1 = new Thread(JanelaProfessor);
-                }
+                    Login = login,
+                    Password = password
+                };
 
-                t1.SetApartmentState(ApartmentState.STA);
-                t1.Start();
+                var client = new HttpClient();
+                client.BaseAddress = new Uri("https://localhost:44303/");
+                var request = client.PostAsync("Login/", new StringContent(JsonConvert.SerializeObject(user), System.Text.Encoding.UTF8, "application/json"));
+                request.Wait();
+                var result = request.Result.Content.ReadAsStringAsync();
+                result.Wait();
+                var resultado = JsonConvert.DeserializeObject<PadraoResult<Usuario>>(result.Result);
+
+                if (!resultado.Error)
+                {
+                    user = resultado.Data[0];
+                    this.Close();
+                    if (user.TipoUsuario == TipoUsuarioEnum.ALUNO)
+                    {
+                        t1 = new Thread(JanelaAluno);
+                    }
+                    if (user.TipoUsuario == TipoUsuarioEnum.ADMINISTRADOR)
+                    {
+                        t1 = new Thread(JanelaAdministrador);
+                    }
+                    if (user.TipoUsuario == TipoUsuarioEnum.PROFESSOR)
+                    {
+                        t1 = new Thread(JanelaProfessor);
+                    }
+
+                    t1.SetApartmentState(ApartmentState.STA);
+                    t1.Start();
+                }
+                else
+                {
+                    lbl_resposta.Text = resultado.Message[0];
+                }
             }
-            else
+            catch (Exception error)
             {
-                lbl_resposta.Text = resultado.Message[0];
+                lbl_resposta.Text = error.Message;
             }
+
 
         }
 
         private void JanelaAluno(object obj)
         {
-            Application.Run(new AlunoView(user) );
+            Application.Run(new AlunoView(user));
         }
 
         private void JanelaAdministrador(object obj)
